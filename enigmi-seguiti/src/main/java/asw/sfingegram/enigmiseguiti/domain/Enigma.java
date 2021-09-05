19ce7dcd0f7b328572411534b4a637a22bce7121
Alessandro <alexzeta97@gmail.com>
2021-09-05 23:43:13 +0200

    Aggiungi base di dati per il servizio "enigmi-seguiti"
    Ho aggiunto la base di dati per il servizio enigmi seguiti, convertendo
    in entità persistenti gli oggetti di dominio definiti nel sotto-package
    "domain".

--- enigmi-seguiti/src/main/java/asw/sfingegram/enigmiseguiti/domain/Enigma.java
+++ enigmi-seguiti/src/main/java/asw/sfingegram/enigmiseguiti/domain/Enigma.java
@@ -1,12 +1,16 @@
 package asw.sfingegram.enigmiseguiti.domain;
 
+import javax.persistence.*; 
 import lombok.*; 
 
 /* Enigma (in formato breve, senza soluzione). */ 
+@Entity
 @Data @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Enigma implements Comparable<Enigma> {
 
+	@Id
+	@GeneratedValue
 	@EqualsAndHashCode.Include
 	private Long id; 
 	private String autore; 
@@ -14,9 +18,16 @@ public class Enigma implements Comparable<Enigma> {
 	private String titolo; 
 	private String[] testo; 
 
+	public Enigma(String autore, String tipo, String titolo, String[] testo) {
+		this();
+		this.autore = autore; 
+		this.tipo = tipo; 
+		this.titolo = titolo; 
+		this.testo = testo; 
+	}
+
 	@Override
 	public int compareTo(Enigma other) {
 		return this.id.compareTo(other.id); 
 	}
-	
 }
